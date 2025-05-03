d27b585607b26066c80658f3eae0c7d9d0b5ae3f
aaafeig <geroy.ozhog08@mail.ru>
2025-05-03 21:05:14 +0300

    сделал добавление вакансии, удаление и поиск вакансии в json файле

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.vacancy import CreateVacancy
+from src.vacancy import Vacancy
 
 def main():
     name = input('Название вакансии: ')
@@ -7,7 +7,7 @@ def main():
     salary = {"salary": {"from": user_input_sal, "currency": user_input_cur}}
     user_input_des = input('Опишите основные свойства вакансии: ')
     description = {"snippet": {"responsibility": user_input_des}}
-    new_vacancy = CreateVacancy(name, salary, description)
+    new_vacancy = Vacancy(name, salary, description)
 
     print(new_vacancy.name)
     print(new_vacancy.salary)
--- src/vacancy.py
+++ src/vacancy.py
@@ -1,62 +1,86 @@
 import json
 import secrets
-from .abstract_classes import BaseVacancy
+from .abstract_classes import BaseVacancy, BaseApi
 
 
-class CreateVacancy(BaseVacancy):
+class Api(BaseApi):
+
+    def __init__(self, api):
+        self.api = api
+        super().__init__()
+
+
+class Vacancy(BaseVacancy):
 
     def __init__(
         self,
         name: str,
         salary: dict,
         description: dict,
+        url: str
     ):
         super().__init__()
-        if not salary['salary']['from'] or salary['salary']['from'] <= 0:
-            raise ValueError("Зарплата не может быть равна нулю или ниже")
+        if not name:
+            raise ValueError("Название вакансии обязательно")
+        if salary and salary.get('from') and salary['from'] < 0:
+            raise ValueError("Зарплата не может быть отрицательной")
 
         self.name = name
         self.salary =  salary['salary']['from']
         self.currency = salary["salary"]["currency"]
         self.description = description.get('snippet', {}).get('responsibility', 'Нет описания')
-        self.id =  secrets.randbelow(1_000_000)
+        self.url = url
+        self.dictForJson = {
+                "title": self.name,
+                "salary": self.salary,
+                "description": self.description,
+                "url": self.url
+            }
 
 
+class JsonSaver:
 
-
-class AddVacancyToJson:
-    def __init__(self, path_json: str, vacancy: CreateVacancy):
+    def __init__(self, path_json, vacancy: Vacancy):
         self.path_json = path_json
         self.vacancy = vacancy
+        with open(self.path_json, 'r', encoding='utf-8') as file:
+            self.data = json.load(file)['items']
+
 
     def add_vacancy(self):
-        with open(self.path_json, "w", encoding="utf-8") as file:
-            pass
+        self.data.append(self.vacancy)
+        self._save_to_file()
+        print(f"Вакансия '{self.vacancy.name}' была добавлена")
 
-class DeleteVacancy:
-
-    def __init__(self, path_json, index):
-        self.pathJson = path_json
-        self.id = index
-
-    def delete_vacancy(self):
-        pass
+    def _save_to_file(self):
+        with open(self.path_json, 'w', encoding='utf-8') as file:
+            json.dump(self.data, file, ensure_ascii=False, indent=2)
 
 
-class ShowVacancies:
+    def delete_vacancy(self, del_vacancy):
+        self.data = [v for v in self.data if v != del_vacancy]
+        self._save_to_file()
+        print(f"Вакансия '{self.vacancy.name}' была удаленна")
 
-    def __init__(self, path_json: str):
-        self.pathJson = path_json
-        with open(self.pathJson, encoding="utf-8") as file:
-            self.dictVacancy = json.load(file)["items"]
+    def show_vacancy(self):
+        for vacancy in self.data:
+            print(
+                f"ID: {vacancy.get('id', 'N/A')}\n"
+                f"Название: {vacancy.get('name', 'N/A')}\n"
+                f"Зарплата: {vacancy.get('salary')}\n"
+                f"Описание: {vacancy.get('snippet', {}).get('responsibility', 'N/A')}\n"
+                "----------------------------------------"
+            )
 
-    def show(self):
 
-        a = [
-            f"название: {vacancy['name']}, описание: {vacancy['snippet']['responsibility']}, зарплата: {f'от {vacancy["salary"]["from"]} {vacancy["salary"]["currency"]}' if vacancy.get('salary') else 'n/a'}"
-            for vacancy in self.dictVacancy
-        ]
-
-        for i in range(len(a)):
-            print(a[i])
+    def search_vacancy(self, name):
+        for vacancy in self.data:
+            if name in vacancy.get("name"):
+                print(
+                    f"ID: {vacancy.get('id')}\n"
+                    f"Название: {vacancy.get('name', 'N/A')}\n"
+                    f"Зарплата: {vacancy.get('salary')}\n"
+                    f"Описание: {vacancy.get('snippet', {}).get('responsibility', 'N/A')}\n"
+                    "----------------------------------------"
+                )
 
