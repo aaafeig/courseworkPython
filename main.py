89d7be8dd8bbb32708904d239041b9dafea80bad
aaafeig <geroy.ozhog08@mail.ru>
2025-05-05 10:00:00 +0300

    -

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-from src.vacancy import Vacancy
+from src.vacancy import Vacancy, JsonSaver
 
 def main():
+    saver = JsonSaver("data/example.json")
+    while True:
+        user_choice = int(input("""Выберите действие:
+        1. Просмотр анкет
+        2. Поиск анкеты
+        3. показать топ N вакансий с зарплатами
+        4. Создание анкеты
+        5. Удаление анкеты
+        6. Закрыть\n"""))
+
+        if user_choice == 1:
+            saver.show_vacancy()
+        elif user_choice == 2:
+            user_search = input("Введите название вакансии ")
+            saver.search_vacancy_name(user_search)
+        elif user_choice == 3:
+            user_number = int(input("Напишите количество вакансий: "))
+            saver.top_n(user_number)
+        elif user_choice == 4:
+            vacancy = create_new_vacancy()
+            saver.add_vacancy(vacancy)
+        elif user_choice == 5:
+            user_delete = input("Введите название вакансии для удаления ")
+            saver.delete_vacancy(user_delete)
+        elif user_choice == 6:
+            break
+        else:
+            print("Введите правильное число")
+
+def create_new_vacancy() -> Vacancy:
     name = input('Название вакансии: ')
     user_input_sal = int(input('Введите начальную зарплату: '))
     user_input_cur = input('Введите валюту: ')
@@ -8,12 +38,7 @@ def main():
     user_input_des = input('Опишите основные свойства вакансии: ')
     description = {"snippet": {"responsibility": user_input_des}}
     new_vacancy = Vacancy(name, salary, description)
-
-    print(new_vacancy.name)
-    print(new_vacancy.salary)
-    print(new_vacancy.currency)
-    print(new_vacancy.description)
-    print(new_vacancy.id)
+    return new_vacancy
 
 if __name__ == "__main__":
     main()
--- src/vacancy.py
+++ src/vacancy.py
@@ -17,50 +17,46 @@ class Vacancy(BaseVacancy):
         name: str,
         salary: dict,
         description: dict,
-        url: str
     ):
         super().__init__()
         if not name:
             raise ValueError("Название вакансии обязательно")
-        if salary and salary.get('from') and salary['from'] < 0:
+        if salary and salary.get("from") and salary["from"] < 0:
             raise ValueError("Зарплата не может быть отрицательной")
 
         self.name = name
-        self.salary =  salary['salary']['from']
+        self.salary = salary["salary"]["from"]
         self.currency = salary["salary"]["currency"]
-        self.description = description.get('snippet', {}).get('responsibility', 'Нет описания')
-        self.url = url
+        self.description = description.get("snippet", {}).get(
+            "responsibility", "Нет описания"
+        )
         self.dictForJson = {
-                "title": self.name,
-                "salary": self.salary,
-                "description": self.description,
-                "url": self.url
-            }
+            "name": self.name,
+            "salary": {"from": self.salary, "currency": self.currency},
+            "snippet": {"responsibility": self.description},
+        }
 
 
 class JsonSaver:
 
-    def __init__(self, path_json, vacancy: Vacancy):
+    def __init__(self, path_json):
         self.path_json = path_json
-        self.vacancy = vacancy
-        with open(self.path_json, 'r', encoding='utf-8') as file:
-            self.data = json.load(file)['items']
+        with open(self.path_json, "r", encoding="utf-8") as file:
+            self.data = json.load(file)
 
-
-    def add_vacancy(self):
-        self.data.append(self.vacancy)
+    def add_vacancy(self, vacancy: Vacancy):
+        self.data.append(vacancy.dictForJson)
         self._save_to_file()
-        print(f"Вакансия '{self.vacancy.name}' была добавлена")
+        print(f"Вакансия '{vacancy.name}' была добавлена")
 
     def _save_to_file(self):
-        with open(self.path_json, 'w', encoding='utf-8') as file:
+        with open(self.path_json, "w", encoding="utf-8") as file:
             json.dump(self.data, file, ensure_ascii=False, indent=2)
 
-
-    def delete_vacancy(self, del_vacancy):
-        self.data = [v for v in self.data if v != del_vacancy]
+    def delete_vacancy(self, del_vacancy: str):
+        self.data = [v for v in self.data if v["name"] != del_vacancy]
         self._save_to_file()
-        print(f"Вакансия '{self.vacancy.name}' была удаленна")
+        print(f"Вакансия '{del_vacancy}' была удаленна")
 
     def show_vacancy(self):
         for vacancy in self.data:
@@ -72,15 +68,32 @@ class JsonSaver:
                 "----------------------------------------"
             )
 
+    def top_n(self, n: int):
+        print(sorted(
+            [v for v in self.data if v],
+            key=lambda x: (
+                x.get('salary', {}).get('from', 0)
+                if x and isinstance(x, dict) and isinstance(x.get('salary'), dict)
+                else 0
+            ),
+            reverse=True
+        )[:n])
 
-    def search_vacancy(self, name):
+    def search_vacancy_name(self, search_word):
         for vacancy in self.data:
-            if name in vacancy.get("name"):
+            name = vacancy.get("name", "")
+            snippet = vacancy.get("snippet", {})
+            responsibility = snippet.get("responsibility", "")
+
+
+            if (name and search_word.lower() in name.lower()) or \
+                    (responsibility and search_word.lower() in responsibility.lower()):
                 print(
-                    f"ID: {vacancy.get('id')}\n"
-                    f"Название: {vacancy.get('name', 'N/A')}\n"
-                    f"Зарплата: {vacancy.get('salary')}\n"
-                    f"Описание: {vacancy.get('snippet', {}).get('responsibility', 'N/A')}\n"
+                    f"ID: {vacancy.get('id', 'N/A')}\n"
+                    f"Название: {name}\n"
+                    f"Зарплата: {vacancy.get('salary', 'N/A')}\n"
+                    f"Описание: {responsibility}\n"
                     "----------------------------------------"
                 )
+
 
